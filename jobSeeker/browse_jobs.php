c76f86df65a23058feb2029d3bb9edd4f01b2f4b
rutika005 <134037361+rutika005@users.noreply.github.com>
2025-04-09 13:14:58 +0530

    add one page.

--- jobSeeker/browse_jobs.php
+++ jobSeeker/browse_jobs.php
@@ -22,44 +22,45 @@
 </head>
 
 <body id='page-top'>
-<?php include('sidebar.php'); ?>
-<?php include('header.php'); ?>
+    <?php include('sidebar.php'); ?>
+    <?php include('header.php'); ?>
 
-<div class='container-fluid'>
-    <h1 class='h3 mb-4 text-gray-800'>Blank Page</h1>
-</div>
+    <div class='container-fluid'>
+        <h1 class='h3 mb-4 text-gray-800'>Blank Page</h1>
+    </div>
 
-<?php include_once('footer.php'); ?>
+    <?php include_once('footer.php'); ?>
 
-<a class='scroll-to-top rounded' href='#page-top'>
-    <i class='fas fa-angle-up'></i>
-</a>
+    <a class='scroll-to-top rounded' href='#page-top'>
+        <i class='fas fa-angle-up'></i>
+    </a>
 
-<div class='modal fade' id='logoutModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-    <div class='modal-dialog' role='document'>
-        <div class='modal-content'>
-            <div class='modal-header'>
-                <h5 class='modal-title' id='exampleModalLabel'>Ready to Leave?</h5>
-                <button class='close' type='button' data-dismiss='modal' aria-label='Close'>
-                    <span aria-hidden='true'>×</span>
-                </button>
-            </div>
-            <div class='modal-body'>Select 'Logout' below if you are ready to end your current session.</div>
-            <div class='modal-footer'>
-                <button class='btn btn-secondary' type='button' data-dismiss='modal'>Cancel</button>
-                <a class='btn btn-success' href='http://localhost/worklink/jobSeeker/logout.php'>Logout</a>
+    <div class='modal fade' id='logoutModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+        <div class='modal-dialog' role='document'>
+            <div class='modal-content'>
+                <div class='modal-header'>
+                    <h5 class='modal-title' id='exampleModalLabel'>Ready to Leave?</h5>
+                    <button class='close' type='button' data-dismiss='modal' aria-label='Close'>
+                        <span aria-hidden='true'>×</span>
+                    </button>
+                </div>
+                <div class='modal-body'>Select 'Logout' below if you are ready to end your current session.</div>
+                <div class='modal-footer'>
+                    <button class='btn btn-secondary' type='button' data-dismiss='modal'>Cancel</button>
+                    <a class='btn btn-success' href='http://localhost/worklink/jobSeeker/logout.php'>Logout</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<script src='vendor/jquery/jquery.min.js'></script>
-<script src='vendor/bootstrap/js/bootstrap.bundle.min.js'></script>
-<script src='vendor/jquery-easing/jquery.easing.min.js'></script>
-<script src='js/sb-admin-2.min.js'></script>
-<script src='vendor/datatables/jquery.dataTables.min.js'></script>
-<script src='vendor/datatables/dataTables.bootstrap4.min.js'></script>
-<script src='js/demo/datatables-demo.js'></script>
+    <script src='vendor/jquery/jquery.min.js'></script>
+    <script src='vendor/bootstrap/js/bootstrap.bundle.min.js'></script>
+    <script src='vendor/jquery-easing/jquery.easing.min.js'></script>
+    <script src='js/sb-admin-2.min.js'></script>
+    <script src='vendor/datatables/jquery.dataTables.min.js'></script>
+    <script src='vendor/datatables/dataTables.bootstrap4.min.js'></script>
+    <script src='js/demo/datatables-demo.js'></script>
 
 </body>
+
 </html>
